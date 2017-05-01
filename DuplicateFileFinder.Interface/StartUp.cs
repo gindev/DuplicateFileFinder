eb56a5f824833d6a58db96cbd5ab75c5c5ca8dad
Panayot Gindev <gindev@live.com>
2017-05-01 16:24:26 +0300

    Described the object of FSEntity (File System Entity). Renamed the form file and class. Changed the form title. Renamed Program.cs to StartUp.cs

--- DuplicateFileFinder.Interface/StartUp.cs
+++ DuplicateFileFinder.Interface/StartUp.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DuplicateFileFinder.Interface
 {
-    static class Program
+    static class StartUp
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +13,7 @@ namespace DuplicateFileFinder.Interface
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
---
+++ DuplicateFileFinder.Models/FSEntity.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace DuplicateFileFinder.Models
+{
+    public class FSEntity
+    {
+        public string Name { get; set; }
+
+        public string Hash { get; set; }
+
+        public bool IsFolder { get; set; }
+
+        public ICollection<FSEntity> Children { get; set; }
+
+        public FSEntity Parent { get; set; }
+
+        public FSEntity()
+        {
+            this.IsFolder = false;
+            this.Children = new HashSet<FSEntity>();
+            this.Parent = null;
+        }
+
+        public FSEntity(string name, string hash, bool isFolder, ICollection<FSEntity> children, FSEntity parent)
+            : this()
+        {
+            this.Name = name;
+            this.Hash = hash;
+
+            if (isFolder)
+            {
+                this.IsFolder = isFolder;
+            }
+
+            if(children != null)
+            {
+                this.Children = children;
+            }
+
+            if(parent != null)
+            {
+                this.Parent = parent;
+            }
+        }
+    }
+}
